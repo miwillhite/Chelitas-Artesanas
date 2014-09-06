942e24d94736b4ce0a65998fe67decf1ca1c0f3e
Matt Willhite <matthew.willhite@gmail.com>
2014-09-06 17:33:35 -0500

    Initial Commit

---
+++ Chelitas Artesanas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chelitas Artesanas
+//
+//  Created by Matthew Willhite on 9/6/14.
+//  Copyright (c) 2014 Notion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
